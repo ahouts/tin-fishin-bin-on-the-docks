5119b258795aef3dd1aaac1471ccbd3befc5593d
Andrew Houts <ahouts4@gmail.com>
2022-07-07 12:36:03 -0500

    improve safety for future api changes

--- modmain.lua
+++ modmain.lua
@@ -1,12 +1,18 @@
 AddRecipePostInit("fish_box", function(existing)
-    local previous_testfn = existing.testfn
+    local util = require("dst-util/util")(GLOBAL)
+
+    local previous_testfn = util.access(existing, "testfn")
+    local monkey_dock = util.access(GLOBAL, "WORLD_TILES", "MONKEY_DOCK")
+    if util.any_nil(previous_testfn, monkey_dock) then
+        util.error("implementation of Tin Fishin' Bin changed, contact mod author!")
+        return
+    end
+
     existing.testfn = function(pt)
-        local world = GLOBAL.TheWorld
-        local monkey_dock = GLOBAL.WORLD_TILES.MONKEY_DOCK
-        local ground_tile = nil
-        if world and monkey_dock then
-            ground_tile = world.Map:GetTileAtPoint(pt.x, pt.y, pt.z)
+        local world = util.access(GLOBAL, "TheWorld")
+        if util.any_nil(world) then
+            util.error("world access changed, contact mod author!")
         end
-        return previous_testfn(pt) or (ground_tile and ground_tile == monkey_dock)
+        return previous_testfn(pt) or world.Map:GetTileAtPoint(pt.x, pt.y, pt.z) == monkey_dock
     end
 end)
